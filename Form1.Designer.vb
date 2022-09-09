538501dbd0c60681afe76daba655c4ba7c43445f
岐 范 <fanqisyx@hotmail.com>
2022-09-09 17:40:27 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(21, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(140, 114)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "unicodeTest"
+        Me.GroupBox1.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(18, 47)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(18, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Location = New System.Drawing.Point(21, 132)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(300, 244)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "单个产品导入"
+        Me.GroupBox2.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(18, 74)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button4)
+        Me.GroupBox3.Controls.Add(Me.Button3)
+        Me.GroupBox3.Controls.Add(Me.TextBox5)
+        Me.GroupBox3.Controls.Add(Me.TextBox6)
+        Me.GroupBox3.Location = New System.Drawing.Point(21, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(624, 521)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(18, 20)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(256, 87)
+        Me.TextBox3.TabIndex = 1
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(18, 113)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(256, 89)
+        Me.TextBox4.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(18, 208)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(521, 472)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "转化"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(20, 34)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(576, 220)
+        Me.TextBox5.TabIndex = 5
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(20, 260)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(576, 206)
+        Me.TextBox6.TabIndex = 4
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(20, 472)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "读取文件"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(659, 548)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = StringToUnicode(TextBox1.Text)
+    End Sub
+
+    '将中文转为unicode编码，如：ZRO 耳麦 12345，转后为：ZRO \u8033\u9EA6 12345
+    Function StringToUnicode(strCode As String) As String
+        Dim a() As String
+        Dim str As String
+        Dim i As Integer
+        For i = 0 To Len(strCode) - 1
+            On Error Resume Next
+            str = Mid(strCode, i + 1, 1)
+            If isChinese(str) = True Then '//是中文
+                'StringToUnicode = StringToUnicode & "&#" & StrDup(4 - Len(Hex(AscW(str))), "0") & Hex(AscW(str))
+                StringToUnicode = StringToUnicode & "&#" & AscW(str) & ";"
+            Else '//不是中文
+                StringToUnicode = StringToUnicode & str
+            End If
+        Next
+    End Function
+
+    '是否为中文
+    Public Function isChinese(Text As String) As Boolean
+
+        Dim l As Long
+        Dim i As Long
+        l = Len(Text)
+        isChinese = False
+
+        For i = 1 To l
+            If Asc(Mid(Text, i, 1)) < 0 Or Asc(Mid(Text, i, 1)) < 0 Then
+                isChinese = True
+                Exit Function
+            End If
+        Next
+
+    End Function
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Private Structure productinfo
+        Dim name As String
+        Dim code As String
+        Dim model As String
+        Dim allunit As String
+        Dim allnum As Integer
+        Dim finishunit As String
+        Dim finishnum As Integer
+        Dim uid As String
+        Dim miaoshu As String
+    End Structure
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim str As String = TextBox3.Text
+        Dim a As productinfo = getinfoFromString(str)
+        TextBox4.Text = Getoutputstring(a)
+        My.Computer.Clipboard.SetText(TextBox4.Text)
+    End Sub
+    Private Function Getoutputstring(a As productinfo) As String
+        Dim outputstr As String
+        outputstr = String.Format("<products assetTypeEnumId=""AstTpInventory"" 
+assetClassEnumId=""AsClsInventory"" 
+description=""{2}"" 
+productName=""{0}"" 
+pseudoId=""{1}"" 
+amountUomId=""OTH_ea"" 
+amountFixed=""2"" 
+lastUpdatedStamp=""1656903551196"" 
+productId=""{1}"" 
+salesIntroductionDate=""1656903479772"" 
+productTypeEnumId=""PtAsset"" 
+ownerPartyId=""218921fe-a65a-4af6-bf2a-710f4fc1e49f"" 
+statusId=""ProdCreated""/>", StringToUnicode(a.name), a.uid, StringToUnicode(a.miaoshu))
+        Return outputstr
+    End Function
+    Private Function getinfoFromString(str) As productinfo
+        Dim arraystr() As String = str.Split(",")
+        If arraystr.Length < 7 Then
+            MessageBox.Show(str)
+        End If
+        Dim a As productinfo = New productinfo
+        a.name = arraystr(1)
+        a.code = arraystr(0)
+        a.model = arraystr(2)
+        a.allnum = GetStringNum(arraystr(4))
+        a.allunit = arraystr(3)
+        a.finishunit = arraystr(5)
+        a.finishnum = GetStringNum(arraystr(6))
+
+        a.uid = getuid()
+        a.miaoshu = "我描述我自己"
+        Return a
+    End Function
+    Private Function GetStringNum(str As String) As Integer
+        Dim outputnum As Integer
+        If IsNumeric(str) = True Then
+            outputnum = Val(str)
+        Else
+            outputnum = 0
+        End If
+        Return outputnum
+    End Function
+
+    Private Function getuid() As String
+        Dim g As Guid = Guid.NewGuid()
+        Return g.ToString()
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim str() As String = TextBox5.Text.Split(vbNewLine)
+        'MessageBox.Show(str(3).ToString())
+        Dim outputstring As String = ""
+        Try
+            For Each mystr In str
+                If mystr.Contains(",") = False Then Continue For
+                Dim a As productinfo = getinfoFromString(mystr)
+                outputstring = outputstring + Getoutputstring(a) + vbNewLine
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+
+        TextBox6.Text = outputstring
+        My.Computer.Clipboard.SetText(TextBox6.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            TextBox5.Text = IO.File.ReadAllText(OpenFileDialog1.FileName, System.Text.Encoding.Default)
+        End If
+    End Sub
+End Class
